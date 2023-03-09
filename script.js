2b38174ed0b7c6bd597ed64be648ca8a7b27989b
oakencore <105373906+oakencore@users.noreply.github.com>
2023-03-09 17:00:28 +0700

    Re-wrote HTML, CSS and Javascript

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
-let myLibrary = [];
+const addBookButton = document.querySelector("#add-book-button");
+const addBookSection = document.querySelector(".add-book-section");
+const bookCardContainer = document.querySelector("#book-cards");
+const form = document.querySelector("#add-book-form");
 
-// Function that creates the constructor as well as a function within it to display the information of a book
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-      return [this.title, this.author, this.pages, this.read];
-    };
+addBookButton.addEventListener("click", () => {
+  addBookSection.classList.toggle("active");
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const titleInput = document.querySelector("#title");
+  const authorInput = document.querySelector("#author");
+  const pagesInput = document.querySelector("#pages");
+  const readInput = document.querySelector('input[name="read-radio"]:checked');
+
+  if (!titleInput.value || !authorInput.value || !pagesInput.value || !readInput) {
+    console.error("Missing form data");
+    return;
   }
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const read = readInput.value === "yes";
+
+  const bookCard = createBookCard(title, author, pages, read);
+  bookCardContainer.appendChild(bookCard);
+
+  // Clear the form inputs
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.checked = false;
+
+  addBookSection.classList.toggle("active");
+});
+
+function createBookCard(title, author, pages, read) {
+  const card = document.createElement("div");
+  card.classList.add("book-card");
+
+  card.innerHTML = `
+    <h2>${title}</h2>
+    <p>Author: ${author}</p>
+    <p>Pages: ${pages}</p>
+    <p>Read: ${read ? "Yes" : "No"}</p>
+    <button class="delete-button">Delete</button>
+  `;
+
+  card.querySelector(".delete-button").addEventListener("click", () => {
+    card.remove();
+  });
+
+  return card;
 }
-
-function addBookToLibrary() {
-  // Get the input values for the book
-  let title = prompt("Enter the title of the book:");
-  let author = prompt("Enter the author of the book:");
-  let pages = prompt("Enter the number of pages of the book:");
-  let read = prompt("Has the book been read (yes/no)?") === "yes";
-
-  // Create a new Book instance with the input values
-  let newBook = new Book(title, author, pages, read);
-
-  // Add the new book to the library
-  myLibrary.push(newBook);
-}
-
-const addBookButton = document.getElementById('add-book-button')
